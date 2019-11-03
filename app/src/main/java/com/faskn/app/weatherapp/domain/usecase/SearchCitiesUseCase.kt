88bbe3fe8c849ff4d5297ad2554c7047b5baa789
furkanaskin <seeingisknow@gmail.com>
2019-11-03 22:05:46 +0300

    Removed unnecessary AbsentLiveData

--- app/src/main/java/com/faskn/app/weatherapp/domain/usecase/SearchCitiesUseCase.kt
+++ app/src/main/java/com/faskn/app/weatherapp/domain/usecase/SearchCitiesUseCase.kt
@@ -3,7 +3,6 @@ package com.faskn.app.weatherapp.domain.usecase
 import androidx.lifecycle.LiveData
 import com.faskn.app.weatherapp.db.entity.CitiesForSearchEntity
 import com.faskn.app.weatherapp.repo.SearchCitiesRepository
-import com.faskn.app.weatherapp.utils.AbsentLiveData
 import com.faskn.app.weatherapp.utils.UseCaseLiveData
 import com.faskn.app.weatherapp.utils.domain.Resource
 import javax.inject.Inject
@@ -21,15 +20,7 @@ class SearchCitiesUseCase @Inject internal constructor(private val repository: S
         )
     }
 
-    data class SearchCitiesParams(
+    class SearchCitiesParams(
         val city: String = ""
-    ) : Params() {
-        fun <T> ifExists(f: (String) -> LiveData<T>): LiveData<T> {
-            return if (city.isBlank()) {
-                AbsentLiveData.create()
-            } else {
-                f(city)
-            }
-        }
-    }
+    ) : Params()
 }
